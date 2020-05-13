e512929e202ef999ea5d6950508d45fc97165967
Sacha Sedriks <sachased@gmail.com>
2020-05-13 08:07:10 -0700

    code cleanup, incl. removal of mass block of comments for v1 and requirements.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import React, { Component } from 'react';
 import List from './composition/List';
 import './App.css';
 
-/*
-// NOTE RE:PROPS btwn v1 (function Component) & v2 (class Component):
-One difference between render() and a function Component() is how 'props' work. In a function component, 'props' are a parameter, as we've seen. In a class, however, 'props' aren't a parameter anymore, we need to access props using 'this.props'.
-*/
 
-// v2: tomatau solution ///////////////////////////////////////////
 class App extends React.Component {
 
-  // defaultProps will be used 
-  // if no equivalent prop is supplied
   static defaultProps = {
     store: {
       lists: [],
@@ -20,7 +13,7 @@ class App extends React.Component {
   };
 
   render() {
-    const { store } = this.props;   // See NOTE RE:PROPS above
+    const { store } = this.props;   
     return (
       <main class="App">
         <header class="App-header">
@@ -42,65 +35,3 @@ class App extends React.Component {
 }
 
 export default App;
-
-
-// // v1: me ////////////////////////////////////////////////////////
-// function App(props) {
-
-//   let lists = props.store.lists.map((list, index) => {
-
-//     // Filtering allCards by cardIds to get appropriate cards per list. // This was where we got stuck =/ // Thanks Stephen! =D
-//     const raw = props.store.allCards
-//     const allowed = list.cardIds;
-//     const filtered = Object.keys(raw)
-//       .filter(id => allowed.includes(id))
-//       .reduce((obj, id) => {
-//         obj[id] = raw[id];
-//         return obj;
-//       }, {});
-
-//     return (
-//       <List 
-//         key={index}
-//         id={props.store.lists[index].id}
-//         header={props.store.lists[index].header}
-//         storeList={filtered}
-//       ></List>
-//     );
-//   });
-
-//   return (
-//     <main className="App">
-//       <header className="App-header">
-//         <h1>Trelloyes!</h1>
-//       </header>
-//       <div className="App-list">
-//         {lists}
-//         {/* <List storeList={props.store.allCards} /> */}
-//       </div>
-//     </main>
-//   );
-// }
-
-// export default App;
-
-
-
-/* REQUIREMENTS
-
-//The App component should render markup matching the design.html: the main element, a header with the h1 and a div with class 'App-list'.
-
-// The App component will accept 1 prop, store.
-//// The store prop is an object with 2 keys: lists and allCards.
-//// lists is an array of objects.
-//// allCards is an object where each key is a card's ID and the value is the card object with a title and content.
-//// You'll be given a dummy STORE object to pass to your App.
-
-// The App should render a List component for each of the items in the store.lists array.
-
-// Each instance of the List component should be passed 2 props (and a key). The 2 props are header and cards.
-//// The header prop is a string for the List's header.
-//// The cards prop will be an array of card objects.
-You'll need to combine the cardIds array for the list with the allCards object.
-
-*/
--- src/composition/List.js
+++ src/composition/List.js
@@ -2,11 +2,9 @@ import React from "react";
 import Card from "./Card";
 import "./List.css";
 
-// v2: tomatau soluton ////////////////////////////////////////////
+
 class List extends React.Component {
 
-  // defaultProps will be used 
-  // if no equivalent prop is supplied
   static defaultProps = {
     key: '',
     header: '',
@@ -14,80 +12,30 @@ class List extends React.Component {
   };
 
   render() {
-    //const card = this.props;
-  return (
-    <section className="List">
-      <header className="List-header">
-        <h2>{this.props.header}</h2>
-      </header>
-      <div className="List-cards">
-        {/* <Card /> */}
-        {/* FYI: 'cards' defined in App.js <List cards={...} /> */}
-        {this.props.cards.map((card) =>  
-          <Card 
-            key={card.id}
-            title={card.title}
-            content={card.content}
-            
-          />
-        )}
-        <button 
-          type="button" 
-          Nameclass="List-add-button"
-        >
-          + Add Random Card
-        </button>
-      </div>
-    </section>
-  );
+    return (
+      <section className="List">
+        <header className="List-header">
+          <h2>{this.props.header}</h2>
+        </header>
+        <div className="List-cards">
+          {/* <Card /> */}
+          {this.props.cards.map((card) =>  
+            <Card 
+              key={card.id}
+              title={card.title}
+              content={card.content}
+            />
+          )}
+          <button 
+            type="button" 
+            Nameclass="List-add-button"
+          >
+            + Add Random Card
+          </button>
+        </div>
+      </section>
+    );
   }
 }
 
 export default List;
-
-// // v1: me ////////////////////////////////////////////////////////
-// function List(props) {
-//   let cards = Object.keys(props.storeList).map((card, index) => {
-//     return (
-//       <Card
-//         key={index}
-//         id={props.storeList[card].id}
-//         title={props.storeList[card].title}
-//         content={props.storeList[card].content}
-//       ></Card>
-//     );
-//   });
-
-//   return (
-//     <section className="List">
-//       <header className="List-header">
-//         <h2>{props.header}</h2>
-//       </header>
-//       <div className='List-cards'>
-//         {cards}
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default List;
-
-
-
-
-/* REQUIREMENTS
-
-// The List component should render markup matching the design.html: a section with a class of 'List', containing a header and div with class 'List-cards'.
-
-// The List component accepts 2 props: header and cards.
-//// header is a string of the header of the card to render.
-//// cards is an array of card objects. Each object should have a title and content.
-//// These props will be passed in for each List from the App component.
-
-// The List should render a Card component for each of the cards in the cards array prop.
-
-// Each instance of the Card component should be passed 2 props (and a key). The 2 props are title and content.
-//// The title prop is a string for the Card's title.
-//// The content prop is a string of the Card's content.
-
-*/
